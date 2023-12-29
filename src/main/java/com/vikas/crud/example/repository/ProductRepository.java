35d8aa26323999b2ae654f789095af411b02f425
vikas prajapati <vikas.prajapati2027@gmail.com>
2023-12-29 11:50:21 +0530

    all code added

---
+++ src/main/java/com/vikas/crud/example/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.vikas.crud.example.repository;
+
+import com.vikas.crud.example.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Product findByName(String name);
+}
+
---
+++ src/test/java/com/vikas/crud/example/SpringBootCrudExample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.crud.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudExample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
